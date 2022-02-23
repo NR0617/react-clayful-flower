dd0dab60d6596781e98a67f6f60bd0e21c2f4ebd
NR0617 <94212747+NR0617@users.noreply.github.com>
2022-02-23 11:50:25 +0900

    make it better

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import clayful from "clayful/client-js";
+
+import axios from "axios";
+import "./css/auth.css";
+
+clayful.config({
+  client: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjYxMmI0YWVjZGVhZmI1ZDY0ZDNhMzZkOWIzY2ExZGJlMWJiY2M3MGZkYmNiMjdkYTYwOGVjYTc0YzQ0ZTVhZWEiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjQ0OTY5ODkzLCJzdG9yZSI6IkNSQUpGSFc4NUZWVi5SWldGQVdQR0pEN1EiLCJzdWIiOiJVSjJKV0FKUDJGMjcifQ.j8-I-_OQDlGWEWR-UFgYGxvygAUMFLUCZu_TR7wY_dI",
+});
+
+clayful.install("request", require("clayful/plugins/request-axios")(axios));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/BrandPage/BrandPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./BrandPage.css";
+
+
+function BrandPage() {
+    return (
+        <div className="pageWrapper">BrandPage</div>
+    )
+}
+
+export default BrandPage;
