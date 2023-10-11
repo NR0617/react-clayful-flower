cf674468680bb341cda278ca8f782eed9d19feaf
NR0617 <nara20210617@gmail.com>
2023-10-12 07:44:19 +0900

    fix: 버그 수정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import clayful from "clayful/client-js";
 import axios from "axios";
 import "./css/auth.css";
 
-
 clayful.config({
-  client: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjYxMmI0YWVjZGVhZmI1ZDY0ZDNhMzZkOWIzY2ExZGJlMWJiY2M3MGZkYmNiMjdkYTYwOGVjYTc0YzQ0ZTVhZWEiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjQ0OTY5ODkzLCJzdG9yZSI6IkNSQUpGSFc4NUZWVi5SWldGQVdQR0pEN1EiLCJzdWIiOiJVSjJKV0FKUDJGMjcifQ.j8-I-_OQDlGWEWR-UFgYGxvygAUMFLUCZu_TR7wY_dI",
+  client:
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjYxMmI0YWVjZGVhZmI1ZDY0ZDNhMzZkOWIzY2ExZGJlMWJiY2M3MGZkYmNiMjdkYTYwOGVjYTc0YzQ0ZTVhZWEiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjQ0OTY5ODkzLCJzdG9yZSI6IkNSQUpGSFc4NUZWVi5SWldGQVdQR0pEN1EiLCJzdWIiOiJVSjJKV0FKUDJGMjcifQ.j8-I-_OQDlGWEWR-UFgYGxvygAUMFLUCZu_TR7wY_dI",
 });
 
 clayful.install("request", require("clayful/plugins/request-axios")(axios));
@@ -19,13 +18,8 @@ clayful.install("request", require("clayful/plugins/request-axios")(axios));
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -4,84 +4,82 @@ import clayful from "clayful/client-js";
 import { AuthContext } from "../../context/AuthContext";
 
 function LoginPage() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isValid, setIsValid] = useState(true);
-    const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("test@gmail.com");
+  const [password, setPassword] = useState("12345678");
+  const [isValid, setIsValid] = useState(true);
+  const { isAuthenticated } = useContext(AuthContext);
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password && password.length < 8) {
+      setIsValid(false);
+    }
+    var Customer = clayful.Customer;
+
+    var payload = {
+      email,
+      password,
     };
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
+    Customer.authenticate(payload, function (err, result) {
+      if (err) {
+        // Error case
+        console.log(err.code);
+        return;
+      }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (password && password.length < 8) {
-            setIsValid(false);
-        }
-        var Customer = clayful.Customer;
+      var data = result.data;
+      localStorage.setItem("customerUid", data.customer);
+      localStorage.setItem("accessToken", data.token);
+      navigate("/");
+      isAuthenticated();
+    });
+  };
 
-        var payload = {
-            email,
-            password,
-        };
+  return (
+    <div className="pageWrapper">
+      <div className="auth-wrapper">
+        <h1>로그인</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleEmailChange}
+            placeholder="test@gmail.com"
+            type="email"
+            name="email"
+            value={email}
+          />
+          <input
+            onChange={handlePasswordChange}
+            placeholder="12345678"
+            type="password"
+            name="password"
+            value={password}
+          />
+          {isValid ? null : <p>비밀번호는 8자리 이상 입력해주세요</p>}
 
-        Customer.authenticate(payload, function (err, result) {
-            if (err) {
-                // Error case
-                console.log(err.code);
-                return;
-            }
+          <p>이메일 주소를 입력하세요.</p>
 
-            var data = result.data;
-            localStorage.setItem("customerUid", data.customer);
-            localStorage.setItem("accessToken", data.token);
-            navigate("/");
-            isAuthenticated();
-        });
-    };
-
-    return (
-        <div className="pageWrapper">
-            <div className="auth-wrapper">
-                <h1>로그인</h1>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        onChange={handleEmailChange}
-                        // placeholder="Email"
-                        placeholder="test@gmail.com"
-                        type="email"
-                        name="email"
-                        value={email}
-                    />
-                    <input
-                        onChange={handlePasswordChange}
-                        // placeholder="암호"
-                        placeholder="12345678"
-                        type="password"
-                        name="password"
-                        value={password}
-                    />
-                    {isValid ? null : <p>비밀번호는 8자리 이상 입력해주세요</p>}
-
-                    <p>이메일 주소를 입력하세요.</p>
-
-                    <button type="submit">로그인</button>
-                    <Link
-                        to="/register"
-                        style={{ color: "gray", textDecoration: "none" }}
-                    >
-                        {" "}
-                        ID가 없으신가요? 지금 생성.
-                    </Link>
-                </form>
-            </div>
-        </div>
-    );
+          <button type="submit">로그인</button>
+          <Link
+            to="/register"
+            style={{ color: "gray", textDecoration: "none" }}
+          >
+            {" "}
+            ID가 없으신가요? 지금 생성.
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
